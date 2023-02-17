6535ce05d5cb131115b8874b1b3ec9aacce05616
Lynda Banh <lyndabanh@gmail.com>
2023-02-17 13:09:04 -0800

    Initial commit

---
+++ lexer/lexer.go
@@ -0,0 +1,23 @@
+package lexer
+
+type lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPositon >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
